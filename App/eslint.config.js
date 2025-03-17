34ce1ee235218ab5acf9251870815f9135672196
Josh Kautz <josh@joshkautz.com>
2025-03-17 15:37:06 -0500

    Formatting

--- App/eslint.config.js
+++ App/eslint.config.js
@@ -3,22 +3,22 @@ import tseslint from 'typescript-eslint'
 import react from 'eslint-plugin-react'
 
 export default [
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  react.configs.flat.recommended,
-  {
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-uses-react': 'off',
-      'react/prop-types': 'off',
-    },
-  },
-  {
-    ignores: ['dist', 'eslint.config.js', 'tailwind.config.js'],
-  },
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	react.configs.flat.recommended,
+	{
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+		rules: {
+			'react/react-in-jsx-scope': 'off',
+			'react/jsx-uses-react': 'off',
+			'react/prop-types': 'off',
+		},
+	},
+	{
+		ignores: ['dist', 'eslint.config.js', 'tailwind.config.js'],
+	},
 ]
